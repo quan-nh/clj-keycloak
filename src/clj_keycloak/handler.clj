e1e1848884c2db3c5a30541fc95546322b638534
Quan Nguyen <huu2uan@gmail.com>
2019-07-20 16:19:41 +0700

    admin rest api

--- src/clj_keycloak/handler.clj
+++ src/clj_keycloak/handler.clj
@@ -8,10 +8,28 @@
             [ring.middleware.params :refer [wrap-params]]
             [ring.util.response :refer [redirect]]))
 
-(def keycloak-endpoint "http://localhost:8080/auth/realms/master/protocol/openid-connect")
+(def keycloak-url "http://localhost:8080/auth")
+(def openid-connect (str keycloak-url "/realms/master/protocol/openid-connect"))
+(def admin-endpoint (str keycloak-url "/admin"))
 
 (defn- user-info [token]
-  (:body (http/get (str keycloak-endpoint "/userinfo")
+  (:body (http/get (str openid-connect "/userinfo")
+                   {:oauth-token token
+                    :as          :json})))
+
+(defn- groups
+  "Get Groups
+  https://www.keycloak.org/docs-api/6.0/rest-api/#_groups_resource"
+  [token]
+  (:body (http/get (str admin-endpoint "/realms/test/groups")
+                   {:oauth-token token
+                    :as          :json})))
+
+(defn- users
+  "Get Users
+  https://www.keycloak.org/docs-api/6.0/rest-api/#_users_resource"
+  [token]
+  (:body (http/get (str admin-endpoint "/realms/test/users")
                    {:oauth-token token
                     :as          :json})))
 
@@ -20,11 +38,19 @@
              (if-let [{:keys [token expires refresh-token]} (-> req :oauth2/access-tokens :keycloak)]
                (html [:div
                       [:a {:href "/logout"} "logout"]
-                      [:div "hello " (:preferred_username (user-info token))]])
+                      [:div "hello " (:preferred_username (user-info token))]
+                      [:div "groups"
+                       [:ul
+                        (for [group (groups token)]
+                          [:li (:name group)])]]
+                      [:div "users"
+                       [:ul
+                        (for [user (users token)]
+                          [:li (:username user)])]]])
                (html [:a {:href "/oauth2/keycloak"} "login"])))
 
            (GET "/logout" []
-             (-> (redirect (str keycloak-endpoint "/logout?redirect_uri=http://localhost:3000/"))
+             (-> (redirect (str openid-connect "/logout?redirect_uri=http://localhost:3000/"))
                  (assoc-in [:session :ring.middleware.oauth2/access-tokens] nil)))
 
            (route/not-found "Not Found"))
@@ -33,8 +59,8 @@
   (-> app-routes
       (wrap-oauth2
         {:keycloak
-         {:authorize-uri    (str keycloak-endpoint "/auth")
-          :access-token-uri (str keycloak-endpoint "/token")
+         {:authorize-uri    (str openid-connect "/auth")
+          :access-token-uri (str openid-connect "/token")
           :client-id        "clj-keycloak"
           :client-secret    "secret"
           ;:scopes           ["email"]
